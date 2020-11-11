0c0e5359e3389d3697fba8dc6c5f17410a1dc926
Chan Odom <c-odom@beniten.com>
2020-11-11 15:49:27 +0700

    initial commit

---
+++ flash-chat/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flash-chat
+//
+//  Created by Jayz Walker on 11/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
